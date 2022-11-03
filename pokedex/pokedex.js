ed186ee1e077e83933849534e7eb77e451492cc9
Diante Dowdell <diante.dowdell99@gmail.com>
2022-11-03 17:31:58 -0500

    saving work

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -10,7 +10,6 @@ form.addEventListener('submit', (event) => {
 let getPokemon = async (pokemon) => {
     try {
         let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
-        console.log(response.data)
         return response.data;
     } catch (error) {
         console.log(error)
@@ -26,7 +25,6 @@ let loadPokemon = async (pokemon) => {
     document.getElementById('pokemon_img').insertAdjacentHTML('afterbegin', new_pokemon_img)
     let pokemon_ability1 = `Main Ability: ${data.abilities[1].ability.name.toUpperCase()}`
     document.getElementById('main_ability').insertAdjacentHTML('afterbegin', pokemon_ability1)
-    console.log(document.getElementById('pokemon_img'))
     let pokemon_ability2 = `Secondary Ability: ${data.abilities[0].ability.name.toUpperCase()}`
     document.getElementById('secondary_ability').insertAdjacentHTML('afterbegin', pokemon_ability2)
 }
